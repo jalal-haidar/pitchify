77ea0bd438b32ba39404230d3afa3840c1a0d871
Jalal Haidar <jalal99.dev@gmail.com>
2024-11-05 02:50:56 +0500

    Refactor Navbar component: replace img with Image, update logout redirect, and improve button labels

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,44 +1,60 @@
-import { auth, signOut, signIn } from "@/auth";
+// export default Navbar;
 import Link from "next/link";
-import React from "react";
+import Image from "next/image";
+import { auth, signOut, signIn } from "@/auth";
+// import { BadgePlus, LogOut } from "lucide-react";
+// import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Navbar = async () => {
   const session = await auth();
+
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
-          <img src="./logo.png" alt="Logo" width={144} height={30} />
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
         </Link>
 
-        <div className="items-center gap-5 text-black">
+        <div className="flex items-center gap-5 text-black">
           {session && session?.user ? (
             <>
               <Link href="/startup/create">
-                <span>create</span>
+                <span className="max-sm:hidden">Create</span>
+                {/* <BadgePlus className="size-6 sm:hidden" /> */}
               </Link>
 
               <form
                 action={async () => {
                   "use server";
-                  await signOut(); //after logout, redirect to home page
+
+                  await signOut({ redirectTo: "/" });
                 }}
               >
-                <button type="submit">Log out</button>
+                <button type="submit">
+                  <span className="max-sm:hidden">Logout</span>
+                  {/* <LogOut className="size-6 sm:hidden text-red-500" /> */}
+                </button>
               </form>
 
-              <Link href={`/user${session?.id}`}>
-                <span>{session?.user?.name}</span>
+              <Link href={`/user/${session?.id}`}>
+                {/* <Avatar className="size-10">
+                  <AvatarImage
+                    src={session?.user?.image || ""}
+                    alt={session?.user?.name || ""}
+                  />
+                  <AvatarFallback>AV</AvatarFallback>
+                </Avatar> */}
               </Link>
             </>
           ) : (
             <form
               action={async () => {
                 "use server";
+
                 await signIn("github");
               }}
             >
-              <button type="submit">Log in</button>
+              <button type="submit">Login</button>
             </form>
           )}
         </div>
