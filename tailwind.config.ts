4219e6e6cb13cc778ad00342bb593b1171d4e80e
Jalal Haidar <jalal99.dev@gmail.com>
2024-11-06 01:17:29 +0500

    Update homepage layout and styles, add utility classes, and configure Tailwind CSS plugins

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 import tailwindcssAnimate from "tailwindcss-animate";
-import tailwindTypography from "@tailwindcss/typography";
+import typography from "@tailwindcss/typography";
 
 const config: Config = {
   darkMode: ["class"],
@@ -47,8 +47,7 @@ const config: Config = {
       },
     },
   },
-  // plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
-  plugins: [tailwindcssAnimate, tailwindTypography],
+  plugins: [tailwindcssAnimate, typography],
 };
 
 export default config;
