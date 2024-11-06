4693a1f6feb73316c83249b5a525f8cd66608802
Jalal Haidar <jalal99.dev@gmail.com>
2024-11-07 02:08:19 +0500

    Add StartupCard component and integrate it into the homepage layout with sample data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from "../../components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({
@@ -6,7 +7,19 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
-
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "John Doe" },
+      _id: 1,
+      category: "Technology",
+      description: "this is a description",
+      image:
+        "https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-169994-674010.jpg&fm=jpg",
+      title: "we robots",
+    },
+  ];
   return (
     <>
       <section className="pink_container">
@@ -20,6 +33,20 @@ export default async function Home({
         </p>
 
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No Startup found</p>
+          )}
+        </ul>
       </section>
     </>
   );
