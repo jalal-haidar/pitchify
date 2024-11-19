456460094602772441c57d0e2827802572aa1a42
Jalal Haidar <jalal99.dev@gmail.com>
2024-11-20 00:09:36 +0500

    Remove placeholder heading from startup submission page

--- app/(root)/startup/create/page.tsx
+++ app/(root)/startup/create/page.tsx
@@ -6,6 +6,7 @@ const page = () => {
       <section className="pink_container !min-h-[230px]">
         <h1 className="heading">Submit Your Startup</h1>
       </section>
+      <h1>d</h1>
     </>
   );
 };
